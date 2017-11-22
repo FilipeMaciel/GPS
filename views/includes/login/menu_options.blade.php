d4692fcc5afe9ad74bf042efb33b7b7ff606f176
Joaofonsecaf <32721336+Joaofonsecaf@users.noreply.github.com>
2017-11-22 20:13:49 -0200

    Add files via upload

---
+++ views/includes/login/menu_options.blade.php
@@ -0,0 +1,2 @@
+<li><a  onclick="$('#modallogin').modal('open') " >Login</a></li>
+<li><a href="badges.html">Documentos para Download</a></li>
---
+++ views/login.blade.php
@@ -0,0 +1,42 @@
+@extends('includes.login.layout')
+@section('content')
+
+
+    <div id="modallogin" class="modal">
+        <div class="modal-content">
+            <div class="center">
+            <h4>Login</h4>
+            </div>
+
+            <form class="col s8">
+                <div class="column">
+                    <div class="input-field col s6">
+                        <input id="login" name="login" type="text" class="validate">
+                        <label for="login">SIAPE</label>
+                    </div>
+
+                    <div class="input-field col s6">
+                        <input id="senha" name="senha" type="text" class="validate">
+                        <label for="senha">SENHA</label>
+                    </div>
+                    <div class="center">
+                    <a class="waves-effect waves-light btn">Entrar</a>
+                    </div>
+                </div>
+            </form>
+        </div>
+
+    </div>
+
+@endsection
+
+@push('scripts')
+<script>
+
+    $(document).ready(function(){
+        // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+        $('.modal').modal();
+    });
+
+</script>
+@endpush
