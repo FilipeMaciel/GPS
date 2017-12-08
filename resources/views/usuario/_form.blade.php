1202a3330b3aded4dd45e5572e6d5c96b80b8d32
danielprrazevedo <daniel.prr.azevedo@gmail.com>
2017-12-08 20:58:48 -0200

    pagina de formuários
    retorno dos erros

--- resources/views/usuario/_form.blade.php
+++ resources/views/usuario/_form.blade.php
@@ -6,21 +6,42 @@
     </ul>
 @endif
 
+
 <div class="row">
-    <div class="input-field col s12 m6">
-        <input type="text" name="nome" value="{{$usuario->nome}}">
+    <div class="input-field col s12 m6 has-error">
+        <input type="text" name="nome" value="{{$error->has('nome') ? old('nome') : $usuario->nome}}">
         <label for="nome">Nome</label>
+        @if ($errors->has('nome'))
+            <span class="has-error">
+                <strong>{{ $errors->first('nome') }}</strong>
+            </span>
+        @endif
     </div>
     <div class="input-field col s12 m6">
-        <input type="text" name="login" value="{{$usuario->login}}">
+        <input type="text" name="login" value="{{$error->has('login') ? old('login') : $usuario->login}}">
         <label for="login">Matricula</label>
+        @if ($errors->has('login'))
+            <span class="has-error">
+                <strong>{{ $errors->first('login') }}</strong>
+            </span>
+        @endif
     </div>
     <div class="input-field col s12 m6">
-        <input type="email" name="email" value="{{$usuario->email}}">
+        <input type="email" name="email" value="{{$error->has('email') ? old('email') : $usuario->email}}">
         <label for="email">Email</label>
+        @if ($errors->has('email'))
+            <span class="has-error">
+                <strong>{{ $errors->first('email') }}</strong>
+            </span>
+        @endif
     </div>
     <div class="input-field col s12 m6">
-        <input type="text" name="cargo" value="{{$usuario->cargo}}">
+        <input type="text" name="cargo" value="{{$error->has('cargo') ? old('cargo') : $usuario->cargo}}">
         <label for="cargo">Cargo</label>
+        @if ($errors->has('cargo'))
+            <span class="has-error">
+                <strong>{{ $errors->first('cargo') }}</strong>
+            </span>
+        @endif
     </div>
 </div>
