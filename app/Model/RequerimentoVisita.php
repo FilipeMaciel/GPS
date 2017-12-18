a5bbe50c69f9057f636b5491b759af7be47cb447
danielprrazevedo <daniel.prr.azevedo@gmail.com>
2017-12-18 20:41:35 -0200

    Commit
    Commit inseguro =]

--- app/Model/RequerimentoVisita.php
+++ app/Model/RequerimentoVisita.php
@@ -4,6 +4,44 @@ namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Model\RequerimentoVisita
+ *
+ * @property int $id
+ * @property int $projeto_id
+ * @property int $coordenacao_id
+ * @property int $campus_id
+ * @property string|null $fone
+ * @property string $local
+ * @property string $percurso
+ * @property int $quilometragem
+ * @property \Carbon\Carbon $chegada
+ * @property \Carbon\Carbon $saida
+ * @property string $motivacao
+ * @property string|null $observacao
+ * @property int $status
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @property-read \App\Model\Campus $campus
+ * @property-read \App\Model\Coordenacao $coordenacao
+ * @property-read \App\Model\ProjetoVisita $projeto
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\RequerimentoVisita whereCampusId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\RequerimentoVisita whereChegada($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\RequerimentoVisita whereCoordenacaoId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\RequerimentoVisita whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\RequerimentoVisita whereFone($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\RequerimentoVisita whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\RequerimentoVisita whereLocal($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\RequerimentoVisita whereMotivacao($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\RequerimentoVisita whereObservacao($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\RequerimentoVisita wherePercurso($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\RequerimentoVisita whereProjetoId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\RequerimentoVisita whereQuilometragem($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\RequerimentoVisita whereSaida($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\RequerimentoVisita whereStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\RequerimentoVisita whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class RequerimentoVisita extends Model
 {
     protected $table = 'requerimentos_visita';
--- app/Model/Usuario.php
+++ app/Model/Usuario.php
@@ -5,6 +5,33 @@ namespace App\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+/**
+ * App\Model\Usuario
+ *
+ * @property int $id
+ * @property string $nome
+ * @property string $login
+ * @property string $password
+ * @property string $email
+ * @property string|null $cargo
+ * @property string|null $remember_token
+ * @property int $status
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Model\ProjetoVisita[] $projetos
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Usuario whereCargo($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Usuario whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Usuario whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Usuario whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Usuario whereLogin($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Usuario whereNome($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Usuario wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Usuario whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Usuario whereStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Usuario whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Usuario extends Authenticatable
 {
     use Notifiable;
